1c2175bd9a35e51870c7c35e2588e6b896b69c05
Aleksandr Osiptsov <solamer@yandex.ru>
2024-03-01 22:08:17 +0300

    Решение финального задания спринт 6 (исправление style ошибок N4)

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -1,10 +1,12 @@
 package service;
 
 import model.Task;
+
 import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
+
     void remove(int id);
 
     List<Task> getHistory();
